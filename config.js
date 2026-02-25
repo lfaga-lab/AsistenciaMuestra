a0a4a3514b09463bf2c81e0e783ea5409c3e9056
lfaga-lab <lfaga@abc.gob.ar>
2026-02-25 19:34:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase config (público / anon) + Edge Function
 window.APP_CONFIG = {
   SUPABASE_URL: "https://etabnqrogxothiojsmxk.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaWVxc2J3cHB0bmZ4bWN3cWxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MjM3MzEsImV4cCI6MjA4NzE5OTczMX0.jKddvteJaHojjl8c_j8oxnhMZiewlGc88t55jMzjls8",
-  FUNCTION_NAME: "asistencia-api"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0YWJucXJvZ3hvdGhpb2pzbXhrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjA1NzgwMiwiZXhwIjoyMDg3NjMzODAyfQ.z-CTYHDNKsNw9-Vu8Kgh9dOWAZgUIx9mVQ-4N6rEVi4",
+  FUNCTION_NAME: "quick-service"
 };
